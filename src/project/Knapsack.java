417b3cc04d23d35b448662d1b336b5e28896444a
이병창 <dig04214@naver.com>
2021-11-27 15:33:06 +0000

    knapsack

--- src/project/Knapsack.java
+++ src/project/Knapsack.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.Math;
 
-public class Main {
+public class Kanpsack {
 
     public static void main(String[] args) throws IOException {
         
